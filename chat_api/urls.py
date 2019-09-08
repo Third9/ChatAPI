362b3833b0a30d809e3d4ea6af342b08574db213
Hyeock Kwon <dev.third9@gmail.com>
2019-09-09 01:01:08 +0900

    챗 목록/보내기 기능 api를 rest형태로 구현

--- chat_api/urls.py
+++ chat_api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import index, room
+from .views import index, room, chat_send, chat_list
 
 urlpatterns = [
     path('', index, name='index'),
     path('<slug:room_name>/', room, name='room'),
+    path('<slug:room_name>/send', chat_send, name='send'),
+    path('<slug:room_name>/list', chat_list, name='send_list')
 ]
--- chat_api/views.py
+++ chat_api/views.py
@@ -1,7 +1,11 @@
 import json
 
+import websocket
+
 from django.shortcuts import render
 from django.utils.safestring import mark_safe
+from django.shortcuts import HttpResponse
+
 
 
 def index(request):
@@ -11,3 +15,30 @@ def room(request, room_name):
     return render(request, 'chat_api/room.html', {
         'room_name_json': mark_safe(json.dumps(room_name))
     })
+
+def chat_send(request, room_name):
+    if request.method == 'GET':
+        import pdb; pdb.set_trace()
+        host = request.get_host()
+        ws = websocket.WebSocket()
+        ws.connect("ws://{}/ws/chat/{}/".format(host, room_name))
+        # ws = websocket.WebSocket("ws://{}/ws/chat/{}/".format(host, room_name))
+
+        send_msg = {"message":"inner method call()"}
+        res = ws.send(json.dumps(send_msg))
+
+        return HttpResponse(status=200, content="test success - " + room_name)
+
+def chat_list(request, room_name):
+    if request.method == 'GET':
+        import pdb; pdb.set_trace()
+        host = request.get_host()
+        ws = websocket.WebSocket()
+        ws.connect("ws://{}/ws/chat/{}/".format(host, room_name))
+        # ws = websocket.WebSocket("ws://{}/ws/chat/{}/".format(host, room_name))
+
+        res = ws.recv()
+        print("=============")
+        print(res)
+
+        return HttpResponse(status=200, content="test success - " + room_name)
